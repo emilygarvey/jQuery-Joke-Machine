89399627114ed9130c2dcd1495c3c8295bc303bb
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2018-12-19 18:29:15 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Joke 1 
+$(      ).click(function(){
+
+});
+
+
+//Joke 2
+$(      ).click(function(){
+
+});
+
+
+//Joke 3
+$(      ).dblclick(function(){
+
+});
+
+
+//Joke 4
+$(      ).click(function(){
+
+});
+
+
+
+
